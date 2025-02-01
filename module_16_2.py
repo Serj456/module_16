c98897429cbe1bb03492619198a20a19221de143
Serj456 <121343943+Serj456@users.noreply.github.com>
2025-02-01 13:35:50 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get('/')
+async def welcome():
+    return {"message": "Hello, world!"}
+
+@app.get('/main')
+async def main():
+    return {"message": "Главная страница"}
+
+@app.get('/user/admin')
+async def admin():
+    return {"message": "Вы вошли как администратор"}
+
+@app.get('/user/{user_id}')
+async def id(user_id: Annotated[int, Path(ge=1,le=100,description='Enter User ID', example='43')]):
+    return {"message": f"Вы вошли как пользователь №{user_id}"}
+
+@app.get('/user/{username}/{age}')
+async def info(username: Annotated[str, Path(min_length=5,max_length=20,description='Enter Username', example='UrbanUser')],
+               age: Annotated[int,Path(ge=18,le=120,description='Enter age', example='33')]):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
